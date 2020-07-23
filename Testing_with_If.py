12dfa87dcf1c4bdfcd1e019debf8b44db232405b
Jackson-Stephens <67711803+Jackson-Stephens@users.noreply.github.com>
2020-07-23 10:31:00 -0700

    Add files via upload

---
+++ Testing_with_If.py
@@ -0,0 +1,10 @@
+# create the string hostname
+hostname = "MTG"
+# test logic with if
+
+hostname = input("What value should be set for hostname? ")
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print("hostname matches expected config")
+    print("Exiting the script")
+
